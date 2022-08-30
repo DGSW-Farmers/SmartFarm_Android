29c4cc30d9c9560d43c20640aca0a4039e38ad8d
LucyMoon <rjf5907@daum.net>
2022-08-30 19:38:28 +0900

    Add: 농장 제어 SlidingUpPanel에 뷰페이저 넣어서 프레그먼트 띄우는 코드 작업

--- app/src/main/java/kr/hs/dgsw/campus/smartfarm/ControlFarmActivity.kt
+++ app/src/main/java/kr/hs/dgsw/campus/smartfarm/ControlFarmActivity.kt
@@ -1,11 +1,78 @@
 package kr.hs.dgsw.campus.smartfarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import kr.hs.dgsw.campus.smartfarm.databinding.ActivityControlFarmBinding
 
-class ControlFarmActivity : AppCompatActivity() {
+class ControlFarmActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityControlFarmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_control_farm)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_control_farm)
+
+        binding.controlVp.adapter = ViewPagerAdapter(this)
+
+        initListener()
+
+    }
+
+
+    private fun initListener() {
+        binding.topLayout.setOnClickListener(this)
+        binding.changeBtn.setOnClickListener(this)
+    }
+
+    private fun changeState() {
+        val state = binding.controlFrame.panelState
+        // 닫힌 상태일 경우 열기
+        if (state == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+            binding.controlFrame.panelState = SlidingUpPanelLayout.PanelState.ANCHORED
+        }
+        // 열린 상태일 경우 닫기
+        else if (state == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            binding.controlFrame.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0) {
+            binding.topLayout -> {
+                changeState()
+            }
+            binding.changeBtn -> {
+                val intent = Intent(this, MainActivity::class.java)
+                finish()
+                startActivity(intent)
+            }
+        }
+    }
+
+    private inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun createFragment(position: Int): Fragment {
+
+            return when (position) {
+                0 -> {
+                    ControlFarmFragment()
+                }
+                1 -> {
+                    StateFarmFirstFragment()
+                }
+                else -> {
+                    StateFarmSecondFragment()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/kr/hs/dgsw/campus/smartfarm/ControlFarmFragment.kt
@@ -0,0 +1,30 @@
+package kr.hs.dgsw.campus.smartfarm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.hs.dgsw.campus.smartfarm.databinding.FragmentControlFarmBinding
+
+
+class ControlFarmFragment : Fragment() {
+    lateinit var binding: FragmentControlFarmBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentControlFarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
